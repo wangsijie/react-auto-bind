83103343d398f8ba3fc5d9fd2756e2ac3ceb77e3
WangSijie <i@sijie.wang>
2018-05-02 16:07:32 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const excludedReactMethods = [
+	'componentWillMount',
+	'render',
+	'componentDidMount',
+	'componentWillReceiveProps',
+	'shouldComponentUpdate',
+	'componentWillUpdate',
+	'componentDidUpdate',
+	'componentWillUnmount',
+	'componentDidCatch',
+	'setState',
+	'forceUpdate'
+];
+
+module.exports = object => {
+
+	for (const key of Object.getOwnPropertyNames(object.constructor.prototype)) {
+		const func = object[key];
+
+		if (key !== 'constructor' && typeof func === 'function' && excludedReactMethods.indexOf(key) === -1) {
+			object[key] = func.bind(object);
+		}
+	}
+
+	return object;
+};
